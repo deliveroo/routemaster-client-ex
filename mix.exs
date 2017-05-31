c355e4d62b2b800f8efbecbf5f8fe2237722bfdb
Tommaso Pavese <tommaso@pavese.me>
2017-05-31 13:39:42 +0100

    ESpec initial configuration

--- mix.exs
+++ mix.exs
@@ -2,12 +2,15 @@ defmodule Routemaster.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :routemaster_client,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :routemaster_client,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      preferred_cli_env: [espec: :test],
+    ]
   end
 
   # Configuration for the OTP application
