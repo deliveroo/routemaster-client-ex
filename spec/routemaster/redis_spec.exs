c1a292fddd4c7c931df53359d86147d7ab8b1c6d
tom <tommaso@pavese.me>
2017-06-03 13:36:43 +0100

    In the redis specs, test with both strings and atoms

--- spec/routemaster/redis_spec.exs
+++ spec/routemaster/redis_spec.exs
@@ -8,13 +8,26 @@ defmodule Routemaster.RedisSpec do
 
   describe "redis commands" do
     describe "get and set" do
-      example "reading a non present value returns nil" do
-        expect Redis.get("foo") |> to(eq {:ok, nil})
+      context "with strings" do
+        example "reading a non present value returns nil" do
+          expect Redis.get("foo") |> to(eq {:ok, nil})
+        end
+
+        it "allows to read previously set key-values" do
+          expect Redis.set("foo", "bar") |> to(eq {:ok, "OK"})
+          expect Redis.get("foo") |> to(eq {:ok, "bar"})
+        end
       end
 
-      it "allows to read previously set key-values" do
-        expect Redis.set("foo", "bar") |> to(eq {:ok, "OK"})
-        expect Redis.get("foo") |> to(eq {:ok, "bar"})
+      context "with atoms" do
+        example "reading a non present value returns nil" do
+          expect Redis.get(:foo) |> to(eq {:ok, nil})
+        end
+
+        it "allows to read previously set key-values" do
+          expect Redis.set(:foo, "bar") |> to(eq {:ok, "OK"})
+          expect Redis.get(:foo) |> to(eq {:ok, "bar"})
+        end
       end
     end
   end
