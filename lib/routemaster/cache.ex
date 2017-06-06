e5c0e9fec4464c5f9a37d1ea8d9ea6c11e5cba2f
tom <tommaso@pavese.me>
2017-06-07 00:42:53 +0100

    remove now redundant key namespace functions from the cache

--- lib/routemaster/cache.ex
+++ lib/routemaster/cache.ex
@@ -6,7 +6,6 @@ defmodule Routemaster.Cache do
 
   # todo: make this configurable or dynamic
   @ttl 3600 # seconds
-  @prefix "cache:"
   @redis Routemaster.Redis.cache()
 
   @doc """
@@ -20,7 +19,7 @@ defmodule Routemaster.Cache do
       {:ok, %{is: "a", good: "fruit"}}
   """
   def read(key) do
-    case @redis.get(ns(key)) do
+    case @redis.get(key) do
       {:ok, nil} ->
         {:miss, nil}
       {:ok, data} ->
@@ -39,7 +38,7 @@ defmodule Routemaster.Cache do
       {:ok, [1, 2, 3]}
   """
   def write(key, term) do
-    case @redis.setex(ns(key), @ttl, serialize(term)) do
+    case @redis.setex(key, @ttl, serialize(term)) do
       {:ok, "OK"} ->
         {:ok, term}
       {:error, _} = error ->
@@ -85,16 +84,12 @@ defmodule Routemaster.Cache do
 
   """
   def clear(key) do
-    case @redis.del(ns(key)) do
+    case @redis.del(key) do
       {:ok, _n} -> :ok # n is 0 or 1
       _ -> :error
     end
   end
 
-
-  defp ns(key) do
-    @prefix <> to_string(key)
-  end
 
   defp serialize(term),   do: :erlang.term_to_binary(term)
   defp deserialize(data), do: :erlang.binary_to_term(data)
