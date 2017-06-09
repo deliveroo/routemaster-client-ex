64fe3c3bf6aeab5e74e24e3b9ddc786254aee281
tom <tommaso@pavese.me>
2017-06-09 18:14:18 +0100

    remove generated hello world function

--- lib/routemaster.ex
+++ lib/routemaster.ex
@@ -2,17 +2,4 @@ defmodule Routemaster do
   @moduledoc """
   Documentation for Routemaster.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Routemaster.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
