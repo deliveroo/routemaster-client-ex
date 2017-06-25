663e7162643f3b74848b55ee4187a1807cd50417
tom <tommaso@pavese.me>
2017-06-25 18:48:10 +0100

    More structure for the basic Drain Plug app

--- lib/routemaster/drain/app.ex
+++ lib/routemaster/drain/app.ex
@@ -5,6 +5,7 @@ defmodule Routemaster.Drain.App do
 
   use Plug.Router
 
+
   if Mix.env == :dev do
     use Plug.Debugger, otp_app: :routemaster
 
@@ -13,11 +14,19 @@ defmodule Routemaster.Drain.App do
     plug Plug.Logger, log: :debug
   end
 
+
+  # reject with 415 non-json requests
+  plug :only_accept_json
+
+
   # Enable to decode JSON bodies
   # plug Plug.Parsers, parsers: [:json]
 
+
+  # required by Plug
   plug :match
   plug :dispatch
+
 
   @doc false
   def init(opts) do
@@ -26,12 +35,34 @@ defmodule Routemaster.Drain.App do
   end
 
 
-  get "/" do
-    send_resp(conn, 200, "")
+  # Main (only?) endpoint, to receive events from the event bus
+  #
+  post "/" do
+    send_resp(conn, 204, "")
   end
 
 
+  # black hole for all other requests
+  #
   match _ do
-    send_resp(conn, 404, "")
+    status = if conn.method == "POST", do: 404, else: 405
+    send_resp(conn, status, "")
+  end
+
+
+  defp only_accept_json(conn, _opts) do
+    if get_req_content_type(conn) == "application/json" do
+      conn
+    else
+      conn |> send_resp(415, "") |> halt()
+    end
+  end
+
+
+  defp get_req_content_type(conn) do
+    conn
+    |> get_req_header("content-type")
+    |> hd()
+    |> String.downcase()
   end
 end
