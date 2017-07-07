58c7331ea423a2b9931b2f6030f5215f186df4fb
tom <tommaso@pavese.me>
2017-07-07 03:06:57 +0100

    reading config from the ENV

--- lib/routemaster/application.ex
+++ lib/routemaster/application.ex
@@ -8,11 +8,13 @@ defmodule Routemaster.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    # load config from the ENV
+    DeferredConfig.populate(:routemaster)
+
     children = [
       Routemaster.Redis.worker_spec(:data),
       Routemaster.Redis.worker_spec(:cache),
     ]
-
 
     opts = [strategy: :one_for_one, name: Routemaster.Supervisor]
     Supervisor.start_link(children, opts)
