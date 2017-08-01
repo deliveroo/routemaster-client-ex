a583b4306746d11e752e7d2c09bf8a833fcf8a3b
tom <tommaso@pavese.me>
2017-08-01 01:56:23 +0100

    alias

--- bench/benchee.exs
+++ bench/benchee.exs
@@ -1,42 +1,45 @@
 # mix run bench/benchee.exs
 
-term = Routemaster.Fetcher.get("https://test.deliveroo.co.uk/api/internal/orders/19123123")
+alias Routemaster.{Cache,Fetcher}
+
+term = Fetcher.get("https://test.deliveroo.co.uk/api/internal/orders/19123123")
 key = "foobar"
 
 Benchee.run(
   %{
     # "serialize_0" => fn() ->
-    #   Routemaster.Cache.serialize_0(term)
+    #   Cache.serialize_0(term)
     # end,
     # "serialize_1" => fn() ->
-    #   Routemaster.Cache.serialize_1(term)
+    #   Cache.serialize_1(term)
     # end,
     # "serialize_9" => fn() ->
-    #   Routemaster.Cache.serialize_9(term)
+    #   Cache.serialize_9(term)
     # end,
 
     "write_0" => fn() ->
-      Routemaster.Cache.write_0(key, term)
+      Cache.write_0(key, term)
     end,
     "write_1" => fn() ->
-      Routemaster.Cache.write_1(key, term)
+      Cache.write_1(key, term)
     end,
     "write_9" => fn() ->
-      Routemaster.Cache.write_9(key, term)
+      Cache.write_9(key, term)
     end,
 
     "write_read_0" => fn() ->
-      Routemaster.Cache.write_0(key, term)
-      Routemaster.Cache.read(key)
+      Cache.write_0(key, term)
+      Cache.read(key)
     end,
     "write_read_1" => fn() ->
-      Routemaster.Cache.write_1(key, term)
-      Routemaster.Cache.read(key)
+      Cache.write_1(key, term)
+      Cache.read(key)
     end,
     "write_read_9" => fn() ->
-      Routemaster.Cache.write_9(key, term)
-      Routemaster.Cache.read(key)
+      Cache.write_9(key, term)
+      Cache.read(key)
     end,
+
   },
   time: 10,
   formatters: [
