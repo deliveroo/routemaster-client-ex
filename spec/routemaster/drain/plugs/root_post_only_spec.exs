930b0941f17436e6cb3ee43f8f231d301062c398
tom <tommaso@pavese.me>
2017-08-22 00:10:01 +0100

    Add a simple Plug that checks HTTP method and request path for the Drain.

---
+++ spec/routemaster/drain/plugs/root_post_only_spec.exs
@@ -0,0 +1,59 @@
+defmodule Routemaster.Drain.Plugs.RootPostOnlySpec do
+  use ESpec, async: true
+  use Plug.Test
+
+  alias Routemaster.Drain.Plugs.RootPostOnly
+
+  @opts RootPostOnly.init([])
+
+  subject RootPostOnly.call(conn(), @opts)
+
+  let :conn, do: conn(method(), path())
+
+  describe "for valid requests (authenticated POST requests to the root path)" do
+    let :method, do: "POST"
+    let :path, do: "/"
+
+    it "returns the unchanged conn" do
+      expect subject() |> to(eq conn())
+    end
+  end
+
+  describe "invalid requests" do
+    describe "for non POST requests to the root path" do
+      let :method, do: "GET"
+      let :path, do: "/"
+
+
+      it "responds with 405" do
+        expect subject().status |> to(eq 405)
+        expect subject().resp_body |> to(be_empty())
+      end
+    end
+
+
+    describe "for any other request (i.e. other verbs AND other paths)" do
+      describe "POST /nope" do
+        let :method, do: "POST"
+        let :path, do: "/nope"
+
+
+        it "responds with 404" do
+          expect subject().status |> to(eq 404)
+          expect subject().resp_body |> to(be_empty())
+        end
+      end
+
+      describe "GET /nope" do
+        let :method, do: "GET"
+        let :path, do: "/nope"
+
+
+        it "responds with 404" do
+          expect subject().status |> to(eq 404)
+          expect subject().resp_body |> to(be_empty())
+        end
+      end
+    end
+  end
+end
