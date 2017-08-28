30e2c749e453cd372af8820b9ea292c93f2ac143
tom <tommaso@pavese.me>
2017-08-28 18:22:39 +0100

    Dedup events in a single drain payload

---
+++ spec/routemaster/drain/plugs/dedup_spec.exs
@@ -0,0 +1,67 @@
+defmodule Routemaster.Drain.Plugs.DedupSpec do
+  use ESpec, async: true
+  use Plug.Test
+
+  alias Routemaster.Drain.Plugs.Dedup
+  alias Routemaster.Drain.Event
+
+  @opts Dedup.init([])
+
+  let :shuffled_events do
+    [
+      %Event{url: "http://foo.bar/1", type: "update", t: 12345678},
+      %Event{url: "http://foo.bar/1", type: "update", t: 12345679},
+      %Event{url: "http://foo.bar/1", type: "update", t: 12345680},
+
+      %Event{url: "http://foo.bar/1", type: "noop", t: 12347777},
+      %Event{url: "http://foo.bar/1", type: "noop", t: 12348000},
+
+      %Event{url: "http://foo.bar/2", type: "update", t: 12345222},
+      %Event{url: "http://foo.bar/2", type: "update", t: 12345223},
+
+      %Event{url: "http://foo.bar/3", type: "noop", t: 12345333},
+    ]
+    |> Enum.shuffle()
+  end
+
+  let :conn do
+    Plug.Conn
+    |> struct()
+    |> Plug.Conn.assign(:events, shuffled_events())
+  end
+
+  subject(
+    Dedup.call(conn(), @opts).assigns.events
+  )
+
+
+  it "removes duplicates by :url and :type" do
+    events = subject()
+
+    expect events |> to(be_list())
+    expect events |> to(have_length 4)
+
+    expected_groups = [
+      {"http://foo.bar/1", "update"},
+      {"http://foo.bar/1", "noop"},
+      {"http://foo.bar/2", "update"},
+      {"http://foo.bar/3", "noop"},
+    ] |> Enum.sort
+    actual_groups = Enum.map(events, &{&1.url, &1.type}) |> Enum.sort
+
+    expect actual_groups |> to(eq expected_groups)
+  end
+
+
+  specify "for each group, only the most recent event is included" do
+    actual_events = subject() |> Enum.sort
+    expected_events = [
+      %Event{url: "http://foo.bar/1", type: "update", t: 12345680},
+      %Event{url: "http://foo.bar/1", type: "noop", t: 12348000},
+      %Event{url: "http://foo.bar/2", type: "update", t: 12345223},
+      %Event{url: "http://foo.bar/3", type: "noop", t: 12345333},
+    ] |> Enum.sort
+
+    expect actual_events |> to(eq expected_events)
+  end
+end
