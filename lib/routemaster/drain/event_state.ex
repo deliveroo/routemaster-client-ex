6d28bd46796658f70b2322f8663b7991e58fdde9
tom <tommaso@pavese.me>
2017-09-08 02:07:04 +0100

    more event_state and ignore_stale plug

--- lib/routemaster/drain/event_state.ex
+++ lib/routemaster/drain/event_state.ex
@@ -1,5 +1,15 @@
 defmodule Routemaster.Drain.EventState do
   @moduledoc """
+  Persisted data.
+
+  Each `EventState` is associated to the URL of a resource and
+  stores the timestamp of the latest Drain event received for
+  that URL.
+
+  They are stored in Redis, with the URL acting as key.
+
+  They are used to keep track of the most recent event for a
+  resource and to filter out stale events receivied out of order.
   """
 
   alias Routemaster.Drain.Event
@@ -19,9 +29,24 @@ defmodule Routemaster.Drain.EventState do
   }
 
 
+  @doc """
+  Checks if a `Routemaster.Drain.Event` is more recent than the
+  previous received events for the same URL (for the same resource).
+
+  In practical terms, it compares the timestamp `r` of the event with
+  the stored timestamp of the most recent previously received event.
+  """
+  @spec fresh?(Event.t) :: boolean
+  def fresh?(%Event{url: url, t: t}) do
+    latest_recorded = get(url)
+    latest_recorded.t < t
+  end
 
 
   @doc """
+  Looks up and `EventState` struct in Redis by URL. If nothing is
+  found, it returns a null struct with timestamp equal to zero
+  which will be considered older than any real data.
   """
   @spec get(Event.url) :: t
   def get(url) do
@@ -35,34 +60,26 @@ defmodule Routemaster.Drain.EventState do
 
 
   @doc """
+  Given a `Routemaster.Drain.Event`, it stores an `EventState` in
+  Redis with the event URL and timestamp.
   """
   @spec save(Event.t) :: :ok | {:error, any}
   def save(%Event{url: url, t: t}) do
     state = %__MODULE__{url: url, t: t}
 
-    case _redis_set(url, state) do
+    case _redis_set(url, serialize(state)) do
       {:ok, _} -> :ok
       {:error, _} = error -> error
     end
   end
 
 
-  @doc """
-  """
-  @spec stale?(Event.t) :: boolean
-  def stale?(%Event{url: url, t: t}) do
-    latest_recorded = get(url)
-    latest_recorded.t >= t
-  end
-
-
-
-  defp _redis_get(url) do
-    @redis.get ns(url)
+  defp _redis_get(key) do
+    @redis.get ns(key)
   end
 
   defp _redis_set(key, value) do
-    @redis.setex ns(key), Config.cache_ttl, serialize(value)
+    @redis.setex ns(key), Config.cache_ttl, value
   end
 
 
---
+++ lib/routemaster/drain/plugs/ignore_stale.ex
@@ -0,0 +1,76 @@
+defmodule Routemaster.Drain.Plugs.IgnoreStale do
+  @moduledoc """
+  Drops stale events from the current payload to only include
+  events that reflect an entity state that is _more recent_ than
+  previously received events.
+  
+  Helps to ignore events received out-of-order (e.g. an `update`
+  event about en entity received after the `delete` event for
+  that same entity), given that the Routemaster event bus server
+  makes no guarantee of in-order delivery of events.
+
+
+  ## Implementation Details
+
+  First it removes duplicates by URL from the current payload
+  and only preserves the most recent event for each URL.
+  Then it compares the timestamp of the remaining events with
+  the Redis-backed `Routemaster.Drain.EventState` data, to
+  check if the event is newer than the latest known state of
+  the resource at that URL.
+  If an event is indeed fresher than the latest known state,
+  the state is updated in Redis with the timestamp of the
+  fresh event.
+
+  Since the output of this middleware should be a single event
+  per URL, filtering the events first and only checking the most
+  recent one will reduce the number of Redis calls.
+
+  The Redis calls to read and save the event states are executed
+  one by one. While reading them in bulk would be more efficient,
+  it would not work correctly because multiple batches can be
+  received and processed concurrently, and out-of-order events
+  for the same resources could be spread in more than one batch.
+  """
+
+  alias Routemaster.Drain.EventState
+  alias Plug.Conn
+
+  def init(opts), do: opts
+
+
+  def call(conn, _opts) do
+    Conn.assign(conn, :events, filter(conn.assigns.events))
+  end
+
+
+  defp filter(events) do
+    events
+    |> newest_by_url()
+    |> remove_stale()
+  end
+
+
+  defp newest_by_url(events) do
+    events
+    |> Enum.group_by(fn(e) -> e.url end)
+    |> Enum.map(fn({_, list}) -> newest(list) end)
+  end
+
+
+  defp newest(list) do
+    Enum.max_by(list, &(&1.t))
+  end
+
+
+  defp remove_stale(events) do
+    Enum.filter events, fn(event) ->
+      if EventState.fresh?(event) do
+        EventState.save(event)
+        true
+      else
+        false
+      end
+    end
+  end
+end
