4be9eb0ab2e7387290298e1080bf2f38dac69148
tom <tommaso@pavese.me>
2017-09-10 19:15:32 +0100

    Create a use'able Drain module

---
+++ lib/routemaster/drain.ex
@@ -0,0 +1,93 @@
+defmodule Routemaster.Drain do
+  defmacro __using__(_opts) do
+    quote do
+      use Plug.Builder
+      alias Routemaster.Drain
+
+
+      use Plug.Builder
+      alias Routemaster.Drain
+
+      if Mix.env == :dev do
+        # Only log in dev, as the host application already
+        # takes care of request logging in production.
+        plug Plug.Logger, log: :debug
+      end
+
+      # Invokes `handle_errors/2` callbacks to handle errors and exceptions.
+      # After the callbacks are invoked the errors are re-raised.
+      # Must be use'd after the debugger.
+      use Plug.ErrorHandler
+
+      plug Drain.Plugs.RootPostOnly
+
+      plug Drain.Plugs.Auth
+
+      # Parse JSON bodies and automatically reject non-JSON requests with a 415 response.
+      plug Drain.Plugs.Parser
+
+      @doc false
+      def init(opts) do
+        Application.ensure_started(:routemaster)
+        super(opts)
+      end
+
+      # Make the compile-time options available to each request.
+      # This allows to customize some nested module with options
+      # passed to the public drain app.
+      #
+      # Available data:
+      #  - conn.assigns.events
+      #  - conn.assigns.drain_opts
+      #
+      @doc false
+      def call(conn, opts) do
+        conn
+        |> assign(:drain_opts, opts)
+        |> super(opts)
+      end
+
+
+      # Either:
+      #  - Plug.Parsers.UnsupportedMediaTypeError
+      #    The request content-type is not JSON.
+      #    Status: 415
+      #  - Plug.Parsers.ParseError
+      #    The request body is not valid JSON
+      #    Status: 400
+      #
+      # This should not normally happen, and it's ok to handle this
+      # with an exception despite the performance hit. The alternative,
+      # that is being defensive and checking the content-type for each
+      # request, is likely going to have a higher performance impact.
+      #
+      defp handle_errors(conn, %{kind: :error, reason: %{plug_status: status}, stack: _}) do
+        send_resp(conn, status, "")
+      end
+
+      defp handle_errors(conn, _), do: conn
+
+
+      # Module.register_attribute(__MODULE__, :drains, accumulate: true)
+      # @before_compile Routemaster.Drain
+    end
+  end
+
+  @doc false
+  defmacro __before_compile__(_env) do
+    # drains = Module.get_attribute(env.module, :drains)
+
+    # {conn, body} = Plug.Builder.compile(env, drains)
+
+    quote do
+      # defp drain_builder_call(unquote(conn), _), do: unquote(body)
+    end
+  end
+
+
+  defmacro drain(drain_module, opts \\ []) do
+    quote do
+      @drains {unquote(drain_module), unquote(opts), true}
+    end
+  end
+end
--- lib/routemaster/drain/app.ex
+++ lib/routemaster/drain/app.ex
@@ -3,72 +3,14 @@ defmodule Routemaster.Drain.App do
   A Plug to receive events over HTTP.
   """
 
-  use Plug.Builder
-  alias Routemaster.Drain
-
-  if Mix.env == :dev do
-    # Only log in dev, as the host application already
-    # takes care of request logging in production.
-    plug Plug.Logger, log: :debug
-  end
-
-  # Invokes `handle_errors/2` callbacks to handle errors and exceptions.
-  # After the callbacks are invoked the errors are re-raised.
-  # Must be use'd after the debugger.
-  use Plug.ErrorHandler
-
-  plug Drain.Plugs.RootPostOnly
-
-  plug Drain.Plugs.Auth
-
-  # Parse JSON bodies and automatically reject non-JSON requests with a 415 response.
-  plug Drain.Plugs.Parser
+  use Routemaster.Drain
+  
 
   plug Drain.Plugs.Dedup
-  plug Drain.Plugs.IgnoreStale
+  # plug Drain.Plugs.IgnoreStale
 
   plug Drain.Plugs.FetchAndCache
 
   plug Drain.Plugs.Terminator
 
-  @doc false
-  def init(opts) do
-    Application.ensure_started(:routemaster)
-    super(opts)
-  end
-
-  # Make the compile-time options available to each request.
-  # This allows to customize some nested module with options
-  # passed to the public drain app.
-  #
-  # Available data:
-  #  - conn.assigns.events
-  #  - conn.assigns.drain_opts
-  #
-  @doc false
-  def call(conn, opts) do
-    conn
-    |> assign(:drain_opts, opts)
-    |> super(opts)
-  end
-
-
-  # Either:
-  #  - Plug.Parsers.UnsupportedMediaTypeError
-  #    The request content-type is not JSON.
-  #    Status: 415
-  #  - Plug.Parsers.ParseError
-  #    The request body is not valid JSON
-  #    Status: 400
-  #
-  # This should not normally happen, and it's ok to handle this
-  # with an exception despite the performance hit. The alternative,
-  # that is being defensive and checking the content-type for each
-  # request, is likely going to have a higher performance impact.
-  #
-  defp handle_errors(conn, %{kind: :error, reason: %{plug_status: status}, stack: _}) do
-    send_resp(conn, status, "")
-  end
-
-  defp handle_errors(conn, _), do: conn
 end
