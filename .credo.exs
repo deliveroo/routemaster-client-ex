34998e6e89b24e9b1a40fdfb8bd1da98f1d785e0
tom <tommaso@pavese.me>
2017-09-12 19:04:13 +0100

    disable alias-checks in credo

--- .credo.exs
+++ .credo.exs
@@ -63,7 +63,7 @@
         # For some checks, like AliasUsage, you can only customize the priority
         # Priority values are: `low, normal, high, higher`
         #
-        {Credo.Check.Design.AliasUsage, priority: :low},
+        {Credo.Check.Design.AliasUsage, false},#priority: :low},
 
         # For others you can set parameters
 
