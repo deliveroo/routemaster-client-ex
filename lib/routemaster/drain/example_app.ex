3396e63162ae35b699fc1fcc0762967d4bfc5893
tom <tommaso@pavese.me>
2017-09-17 18:12:19 +0100

    Update the example drain app to show how to use the siphon drain

--- lib/routemaster/drain/example_app.ex
+++ lib/routemaster/drain/example_app.ex
@@ -10,7 +10,24 @@ defmodule Routemaster.Drain.ExampleApp do
 
   use Routemaster.Drain
 
+  drain Routemaster.Drains.Siphon,
+    topic: "rabbits", to: Routemaster.ExampleRabbitSiphon
+
   drain Routemaster.Drains.Dedup
   drain Routemaster.Drains.IgnoreStale
   drain Routemaster.Drains.FetchAndCache
+end
+
+
+defmodule Routemaster.ExampleRabbitSiphon do
+  @moduledoc false
+
+  require Logger
+
+  def call(events) do
+    Logger.info """
+    [#{__MODULE__}]: #{length(events)} events siphoned:
+    #{inspect events}
+    """
+  end
 end
