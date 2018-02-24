df4f288c89e228a3716062f6e3ef69abbc0cdf72
Tommaso Pavese <tommaso@pavese.me>
2018-02-24 17:54:24 +0100

    The custom parser should work reliably both in phoenix (when the framework already parses the json) and when it has to do its own parsing

--- lib/routemaster/drain/plugs/parser.ex
+++ lib/routemaster/drain/plugs/parser.ex
@@ -7,14 +7,58 @@ defmodule Routemaster.Plugs.Parser do
   """
 
   use Plug.Builder
+  alias Routemaster.Drain.Event
 
-  plug Plug.Parsers, parsers: [:json], json_decoder: Routemaster.Drain.JsonDecoder
+  defmodule InvalidPayloadError do
+    @moduledoc false
+    defexception [:message]
+  end
+
+  plug Plug.Parsers, parsers: [:json], json_decoder: Poison
 
   # When decoding JSON with root-level arrays, Poison will merge in the params
   # with a "_json" key. Here we extract them and move them in the assigns.
   #
-  def call(conn, opts) do
+  def call(conn = %{body_params: %Plug.Conn.Unfetched{}}, opts) do
     conn = super(conn, opts)
-    assign(conn, :events, conn.params["_json"])
+    decode_the_events(conn)
+  end
+
+  def call(conn, _opts) do
+    decode_the_events(conn)
+  end
+
+  defp decode_the_events(conn) do
+    events =
+      conn.params["_json"]
+      |> Enum.map(&cast_and_validate!/1)
+
+    conn
+    |> assign(:events, events)
+    |> clean_the_params()
+  end
+
+
+  defp clean_the_params(conn) do
+    conn = %{conn | body_params: Map.delete(conn.body_params, "_json") }
+    conn = %{conn | params: Map.delete(conn.params, "_json") }
+    conn
+  end
+
+
+  defp cast_and_validate!(map) do
+    event = %Event{
+      type: map["type"],
+      url: map["url"],
+      t: map["t"],
+      topic: map["topic"],
+      data: map["data"]
+    }
+
+    if Event.complete?(event) do
+      event
+    else
+      raise InvalidPayloadError, message: "The events in the received payloads are not complete"
+    end
   end
 end
